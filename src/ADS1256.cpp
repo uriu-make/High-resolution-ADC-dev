82d1ca27ae187c88501e9367e1fd811faa84154b
uriu-make <yoshinori.3775@outlook.jp>
2022-07-22 15:34:40 +0900

    update

--- src/ADS1256.cpp
+++ src/ADS1256.cpp
@@ -11,6 +11,216 @@
 #include "spi.h"
 #include "ADS1256.h"
 
-// int ADS1256::AnalogRead(){
-// ADS1256::fd;
-// }
+int ADS1256::ReadReg(__u8 reg, __u8* value) {
+  __u8 tx[2];
+  struct spi_ioc_transfer arg[2];
+  memset(arg, 0, sizeof(arg));
+
+  tx[0] = 0b00010000 | (reg & 0b00001111);
+  tx[1] = 0;
+
+  arg[0].tx_buf = (__u64)&tx;
+  arg[0].rx_buf = (__u64)NULL;
+  arg[0].len = 2;
+  arg[0].delay_usecs = ADS1256::delay_sclk;
+  arg[0].speed_hz = ADS1256::speed;
+  arg[0].bits_per_word = 8;
+  arg[0].cs_change = 0;
+
+  arg[1].tx_buf = (__u64)value;
+  arg[1].rx_buf = (__u64)NULL;
+  arg[1].len = 1;
+  arg[1].delay_usecs = 0;
+  arg[1].speed_hz = ADS1256::speed;
+  arg[1].bits_per_word = 8;
+  arg[1].cs_change = 0;
+
+  return ADS1256::transfer(arg, 2);
+}
+
+int ADS1256::WriteReg(__u8 reg, __u8 value) {
+  __u8 tx[2];
+  struct spi_ioc_transfer arg[2];
+  memset(arg, 0, sizeof(arg));
+
+  tx[0] = 0b01010000 | (reg & 0b00001111);
+  tx[1] = 0;
+
+  arg[0].tx_buf = (__u64)&tx;
+  arg[0].rx_buf = (__u64)NULL;
+  arg[0].len = 2;
+  arg[0].delay_usecs = ADS1256::delay_sclk;
+  arg[0].speed_hz = ADS1256::speed;
+  arg[0].bits_per_word = 8;
+  arg[0].cs_change = 0;
+
+  arg[1].tx_buf = (__u64)&value;
+  arg[1].rx_buf = (__u64)NULL;
+  arg[1].len = 1;
+  arg[1].delay_usecs = 0;
+  arg[1].speed_hz = ADS1256::speed;
+  arg[1].bits_per_word = 8;
+  arg[1].cs_change = 0;
+
+  return ADS1256::transfer(arg, 2);
+}
+
+int ADS1256::setClock(int clock) {
+  ADS1256::CLOCK = clock;
+  ADS1256::delay_sclk = std::ceil(50 * 1000000 / ADS1256::CLOCK);
+
+  return ADS1256::spiSpeed(ADS1256::CLOCK / 4);
+}
+
+void ADS1256::setVREF(double vref) {
+  ADS1256::VREF = vref;
+}
+
+int ADS1256::setAnalogBuffer(bool buf) {
+  __u8 reg, value;
+  reg = 0b00000000;
+  if (ADS1256::ReadReg(reg, &value) <= 0) {
+    return -1;
+  }
+
+  if (buf == true) {
+    value = (value & 0b11111100) | 0b00000010;
+  } else {
+    value = (value & 0b11111100) | 0b00000000;
+  }
+  return ADS1256::WriteReg(reg, value);
+}
+
+int ADS1256::setSampleRate(__u8 rate) {
+  __u8 reg = 0b00000111;
+  return ADS1256::WriteReg(reg, rate);
+}
+
+int ADS1256::setPGA(__u8 gain) {
+  __u8 reg, value;
+  reg = 0b0000010;
+
+  if (ADS1256::ReadReg(reg, &value) <= 0) {
+    return -1;
+  }
+
+  value = (value & 0b01111000) | (gain & 0b00000111);
+  if (gain > GAIN_64) {
+    gain = GAIN_64;
+  }
+  ADS1256::GAIN = pow(2, gain);
+  return ADS1256::WriteReg(reg, value);
+}
+
+int ADS1256::setClockOUT(__u8 mode) {
+  __u8 reg, value;
+  reg = 0b0000010;
+
+  if (ADS1256::ReadReg(reg, &value) <= 0) {
+    return -1;
+  }
+  value = (value & 0b00011111) | ((mode << 5) & 0b01100000);
+  return ADS1256::WriteReg(reg, value);
+}
+
+int ADS1256::setSDC(__u8 mode) {
+  __u8 reg, value;
+  reg = 0b0000010;
+
+  if (ADS1256::ReadReg(reg, &value) <= 0) {
+    return -1;
+  }
+  value = (value & 0b01100111) | ((mode << 3) & 0b00011000);
+  return ADS1256::WriteReg(reg, value);
+}
+
+int ADS1256::setAIN(__u8 positive, __u8 negative) {
+  __u8 reg, ain;
+  reg = 0b00000001;
+  ain = ((positive << 4) & 0b11110000) | (negative & 0b00001111);
+  return ADS1256::WriteReg(reg, ain);
+}
+
+int ADS1256::pinMode(__u8 pin, __u8 mode) {
+  __u8 reg, value;
+  reg = 0b00000100;
+
+  if (ADS1256::ReadReg(reg, &value) <= 0) {
+    return -1;
+  }
+  value = (value & (~_BITULL(pin + 4))) | ((mode << _BITULL(pin + 4)) | _BITULL(pin + 4));
+  return ADS1256::WriteReg(reg, value);
+}
+
+int ADS1256::digitalRead(__u8 pin) {
+  __u8 reg, value;
+  reg = 0b00000100;
+
+  if (ADS1256::ReadReg(reg, &value) <= 0) {
+    return -1;
+  }
+  return 0b00000001 & (value >> pin);
+}
+
+int ADS1256::digitalWrite(__u8 pin, __u8 value) {
+  __u8 reg, buf;
+  reg = 0b00000100;
+
+  if (ADS1256::ReadReg(reg, &buf) <= 0) {
+    return -1;
+  }
+  buf = ((buf & (~_BITULL(pin))) & 0b11111111) | ((value & 0b00001111) << pin);
+  return ADS1256::WriteReg(reg, buf);
+}
+
+int ADS1256::selfCal(void) {
+  __u8 tx = 0b11110000;
+  struct spi_ioc_transfer reg;
+  memset(&reg, 0, sizeof(reg));
+
+  reg.tx_buf = (__u64)&tx;
+  reg.rx_buf = (__u64)NULL;
+  reg.len = 1;
+  reg.delay_usecs = 0;
+  reg.speed_hz = ADS1256::speed;
+  reg.bits_per_word = 8;
+  reg.cs_change = 0;
+  return ADS1256::transfer(&reg, 1);
+}
+
+double ADS1256::AnalogRead(void) {
+  int buf;
+  buf = ADS1256::AnalogReadRow();
+  if ((buf & _BITULL(23)) > 0) {
+    return -1 * double(buf) * 2 * ADS1256::VREF / (ADS1256::GAIN * 0x7FFFFF);
+  } else {
+    return double(buf) * 2 * ADS1256::VREF / (ADS1256::GAIN * 0x7FFFFF);
+  }
+}
+
+int ADS1256::AnalogReadRow(void) {
+  struct spi_ioc_transfer arg[2];
+  __u8 tx, rx[3];
+
+  memset(arg, 0, sizeof(arg));
+  tx = 0b00000001;
+  memset(rx, 0, sizeof(rx));
+
+  arg[0].tx_buf = (__u64)&tx;
+  arg[0].rx_buf = (__u64)NULL;
+  arg[0].len = 1;
+  arg[0].delay_usecs = ADS1256::delay_sclk;
+  arg[0].speed_hz = ADS1256::speed;
+  arg[0].bits_per_word = 8;
+  arg[0].cs_change = 0;
+
+  arg[1].tx_buf = (__u64)NULL;
+  arg[1].rx_buf = (__u64)&rx;
+  arg[1].len = 3;
+  arg[1].delay_usecs = 0;
+  arg[1].speed_hz = ADS1256::speed;
+  arg[1].bits_per_word = 8;
+  arg[1].cs_change = 0;
+  ADS1256::transfer(arg, 2);
+  return (rx[0] & 0b01111111 << 16) | (rx[1] << 8) | rx[2];
+}
--- src/ADS1256.h
+++ src/ADS1256.h
@@ -13,6 +13,7 @@
 #ifndef __ADS1256_H_
 #define __ADS1256_H_
 
+//サンプリングレート
 #define DATARATE_30000 0b11110000
 #define DATARATE_15000 0b11100000
 #define DATARATE_7500  0b11010000
@@ -30,20 +31,59 @@
 #define DATARATE_5     0b00010011
 #define DATARATE_2_5   0b00000011
 
+// PGAゲイン
+#define GAIN_1  0b000
+#define GAIN_2  0b001
+#define GAIN_4  0b010
+#define GAIN_8  0b011
+#define GAIN_16 0b100
+#define GAIN_32 0b101
+#define GAIN_64 0b110
+
+//クロック出力
+#define CLOCK_OFF   0b00
+#define CLOCK_DIV_1 0b01
+#define CLOCK_DIV_2 0b10
+#define CLOCK_DIV_4 0b11
+
+//開放/短絡センサー検出
+#define SDC_OFF   0b00
+#define SDC_500nA 0b01
+#define SDC_2uA   0b10
+#define SDC_10uA  0b11
+
+// GPIOモード
+#define OUTPUT 0
+#define INPUT  1
+
 class ADS1256 : private spi {
  private:
-  __u8 RATE;
-  __u8 GAIN;
-  double VREF;
-  __u8 ADCPIN;
+  int CLOCK;         // ADS1256に接続された水晶振動子の周波数
+  __u8 GAIN = 1;     // PGAのゲイン
+  double VREF;       //接続された基準電圧
+  __u16 delay_sclk;  //連続した通信に挟む待ち時間
 
  public:
+  int ReadReg(__u8 reg, __u8* value);  //指定レジスタの読み取り
+  int WriteReg(__u8 reg, __u8 value);  //レジスタに書き込む関数
+
+  int setClock(int clock);    // ADS1256に入力される水晶振動子の周波数から、その他の周波数を設定する
+  void setVREF(double vref);  //リファレンス電圧の設定
   int setDRDYpin(int pin);
-  int setDataRate(__u8 rate);
-  int setPGA(int gain);
-  int setVREF(double vref);
-  int setAIN(__u8 positive, __u8 negative);
-  double AnalogRead(void);
+  int setAnalogBuffer(bool buf);             //アナログバッファの有効/無効
+  int setSampleRate(__u8 rate);              //サンプリングレートの設定
+  int setPGA(__u8 gain);                     //プログラマブルゲインアンプの設定
+  int setAIN(__u8 positive, __u8 negative);  //アナログ入力のピンを設定
+  int setClockOUT(__u8 mode);                //クロック出力を設定
+  int setSDC(__u8 mode);                     //開放/短絡センサー検出の設定
+
+  int pinMode(__u8 pin, __u8 mode);        // GPIOピンのモードを設定
+  int digitalRead(__u8 pin);               // GPIO入力
+  int digitalWrite(__u8 pin, __u8 value);  // GPIO出力
+
+  int selfCal(void);        //セルフキャリブレーション
+  double AnalogRead(void);  // ADCの値を電圧で返す
+  int AnalogReadRow(void);  // ADCの値を生で返す
 };
 
 #endif
