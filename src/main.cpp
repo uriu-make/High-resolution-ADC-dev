51a98d89df8920b96b83472c720bdf7822f915e1
uriu-make <yoshinori.3775@outlook.jp>
2022-09-17 13:40:53 +0900

    update

--- src/main.cpp
+++ src/main.cpp
@@ -17,24 +17,24 @@
 #define RESET         18
 #define SYNC          27
 
-// #define SAMPLENUM 10000
-
-struct ADC {
-  double volt;
-  struct timeval time;
-};
+#define SAMPLENUM 20000
 
 struct send_data {
   double volt;
   __u64 t;
 };
 
+struct buffer {
+  int len;
+  struct send_data data[SAMPLENUM];
+};
+
 ADS1256 ads1256("/dev/spidev0.0", "/dev/gpiochip0", DRDY, RESET, SYNC, ADS1256_CLOCK);
 
 class DATA {
  private:
-  std::queue<struct ADC> q;
   std::mutex m;
+  struct buffer buf = {-1, 0};
 
  public:
   void getADC();
@@ -42,6 +42,10 @@ class DATA {
 };
 
 DATA data;
+
+void message() {
+  std::cout << "socket is close" << std::endl;
+}
 
 int main() {
   pid_t pid;
@@ -57,20 +61,7 @@ int main() {
     std::cerr << "ERROR" << std::endl;
     exit(0);
   }
-
-  // int sock_listen = socket(AF_INET, SOCK_STREAM, 0);
-
-  // struct sockaddr_in server_addr = {0};
-  // server_addr.sin_family = AF_INET;
-  // server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  // server_addr.sin_port = htons(60000);
-  // bind(sock_listen, (struct sockaddr*)&server_addr, sizeof(server_addr));
-  // listen(sock_listen, 5);
-  // struct sockaddr_in client_addr;
-  // socklen_t len = sizeof(struct sockaddr_in);
-
-  // int sock = accept(sock_listen, (struct sockaddr*)&client_addr, &len);
-  // printf("accepted.\n");
+  atexit(message);
 
   //初期化
 
@@ -89,6 +80,7 @@ int main() {
   ads1256.drdy_ready();    //校正の終了を待つ
   ads1256.enable_event();  // drdyのイベント検出を有効化
 
+  //ソケット作成
   int sock_listen = socket(AF_INET, SOCK_STREAM, 0);
 
   struct sockaddr_in server_addr = {0};
@@ -100,10 +92,14 @@ int main() {
   struct sockaddr_in client_addr;
   socklen_t len = sizeof(struct sockaddr_in);
 
-  int sock = accept(sock_listen, (struct sockaddr*)&client_addr, &len);
+  int sock;
+  do {
+    sock = accept(sock_listen, (struct sockaddr*)&client_addr, &len);
+  } while (sock <= 0);
   printf("accepted.\n");
+  // usleep(1000);
 
-  std::printf("t,volt\n");
+  // std::printf("t,volt\n");
   std::fflush(stdout);
   std::thread th0{&DATA::getADC, &data};
   std::thread th1{&DATA::sendData, &data, sock};
@@ -112,13 +108,12 @@ int main() {
   th1.join();
 
   ads1256.ADS1256_close();
-  // close(sock);
-  // close(sock_listen);
+  close(sock);
+  close(sock_listen);
   return 0;
 }
 
 void DATA::getADC() {
-  struct ADC buf;
   cpu_set_t cpu_set;
   int result;
   CPU_ZERO(&cpu_set);
@@ -128,13 +123,14 @@ void DATA::getADC() {
     std::cerr << "ERROR" << std::endl;
     exit(0);
   }
-
+  timeval time;
   while (true) {
-    // data[j].raw= ads1256.AnalogReadRawSync();  //同期をとり、セトリング・タイムを制御して測定
-    buf.volt = ads1256.AnalogRead();  //同期を取らずにデータを取る
-    gettimeofday(&buf.time, NULL);
     m.lock();
-    q.push(ADC{buf.volt, buf.time});
+    buf.len++;
+    // data[j].raw= ads1256.AnalogReadRawSync();  //同期をとり、セトリング・タイムを制御して測定
+    buf.data[buf.len].volt = ads1256.AnalogRead();  //同期を取らずにデータを取る
+    gettimeofday(&time, NULL);
+    buf.data[buf.len].t = time.tv_sec * 1000000 + time.tv_usec;
     m.unlock();
   }
 }
@@ -149,28 +145,19 @@ void DATA::sendData(int sock) {
     std::cerr << "ERROR" << std::endl;
     exit(0);
   }
-
-  while (true) {
-    usleep(1000);
+  int i = 100000;
+  while (i > 0) {
+    // for (int i = 0; i < 100; i++) {
     m.lock();
-    int size = q.size();
-    if (size > 0) {
-      struct ADC buf;
-      struct send_data data[size];
-      // m.lock();
-      for (int i = 0; i < size; i++) {
-        buf = q.front();
-        q.pop();
-        data[i].volt = buf.volt;
-        data[i].t = buf.time.tv_sec * 1000000 + buf.time.tv_usec;
-      }
-      m.unlock();
-      write(sock, &data, sizeof(data));
-      // for (int i = 0; i < size; i++) {
-      //   std::printf("%lld,%lf\n", data[i].t, data[i].volt);
-      // }
-    } else {
-      m.unlock();
+    if (buf.len >= 0) {
+      write(sock, buf.data, sizeof(send_data) * buf.len);
+      // std::cout << buf.len << std::endl;
+      i = i - buf.len;
+      buf.len = -1;
+      // memset(buf.data, 0, sizeof(buf.data));
     }
+    m.unlock();
+    usleep(1000);
   }
+  exit(0);
 }
