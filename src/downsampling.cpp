8aa649a84419bf71d1095f2a3dc10a5eb4fdec99
uriu-make <yoshinori.3775@outlook.jp>
2023-01-16 14:50:04 +0900

    add downsampling function

--- src/downsampling.cpp
+++ src/downsampling.cpp
@@ -1,2 +1,28 @@
 #include "downsampling.h"
 
+void DOWNSAMPLING::sampling(double *t, double *v, int len) {
+  static double t_0 = 0.0;
+  static int sum = 0;
+
+  memcpy(buf_t, &buf_t[len], sizeof(double) * len);
+  memcpy(buf_v, &buf_v[len], sizeof(double) * len);
+  memcpy(&buf_t[size - len], t, sizeof(double) * len);
+  memcpy(&buf_v[size - len], v, sizeof(double) * len);
+  if (size > sum) {
+    sum += len;
+  } else {
+    t_0 = buf_t[0];
+
+    for (int i = 0, j = 1, basepoint = 0; i < size; i++) {
+      buf_t[i] = buf_t[i] - t_0;
+
+      if (buf_t[i] >= rate * j) {
+        for (int k = basepoint; k < i; k++) {
+          sample[j - 1] += buf_v[k];
+        }
+        sample[j - 1] /= i - basepoint;
+        basepoint = i;
+      }
+    }
+  }
+}
--- src/downsampling.h
+++ src/downsampling.h
@@ -1,2 +1,37 @@
+#include <complex>
+#include <string.h>
 #pragma once
 
+struct buffer {
+  int32_t len;
+  double volt[10000];
+  int64_t t[10000];
+};
+
+class DOWNSAMPLING {
+ private:
+  int size;
+  int N;
+  double rate;
+  double *buf_v;
+  double *buf_t;
+  double *sample;
+
+ public:
+  DOWNSAMPLING(int size, int N);
+  ~DOWNSAMPLING();
+  void sampling(double *t, double *v, int len);
+  void fft();
+};
+
+DOWNSAMPLING::DOWNSAMPLING(int size, int N) {
+  this->size = size;
+  this->N = N;
+  buf_t = new double[size];
+  buf_v = new double[size];
+  sample = new double[N];
+  rate = 1.0 / static_cast<double>(N);
+}
+
+DOWNSAMPLING::~DOWNSAMPLING() {
+}
